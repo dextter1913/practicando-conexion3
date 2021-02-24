2c26bacf2bd1fb90ae78c45ef4592fd497a75c1a
dextter1913 <Clau32usa@gmai.com>
2021-02-24 17:56:01 -0500

    Actualizacion}

--- index.php
+++ index.php
@@ -7,17 +7,18 @@
     <title>Practicando conexion 3</title>
 </head>
 <body>
-    <center><h1>practicando conexion 3</h1></center>
+    <center><h1>practicando conexion 3</h1>
     <form action="index.php" method="post">
     <label for="id">ID:</label>
-    <input type="text" name="id" id="id">
+    <input type="text" name="id" id="id"><br><br>
     <label for="nombre">Nombre:</label>
-    <input type="text" name="nombre" id="nombre">
+    <input type="text" name="nombre" id="nombre"><br><br>
     <label for="apellido">Apellido:</label>
-    <input type="text" name="apellido" id="apelido">
+    <input type="text" name="apellido" id="apelido"><br><br>
     <label for="telefono">Telefono:</label>
-    <input type="text" name="telefono" id="telefono">
+    <input type="text" name="telefono" id="telefono"><br><br>
     <input type="submit" value="Ingresar">
     </form>
+    </center>
 </body>
 </html>
