b8b38c120e14a1c7254f8fee8d656f16bb77f4c2
dextter1913 <Clau32usa@gmai.com>
2021-02-24 17:52:58 -0500

    Actualizacion

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicando conexion 3</title>
+</head>
+<body>
+    <center><h1>practicando conexion 3</h1></center>
+    <form action="index.php" method="post">
+    <label for="id">ID:</label>
+    <input type="text" name="id" id="id">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="nombre" id="nombre">
+    <label for="apellido">Apellido:</label>
+    <input type="text" name="apellido" id="apelido">
+    <label for="telefono">Telefono:</label>
+    <input type="text" name="telefono" id="telefono">
+    <input type="submit" value="Ingresar">
+    </form>
+</body>
+</html>
